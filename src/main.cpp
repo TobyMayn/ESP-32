a65726ef8b3fe3716266f43abce2ba20796b423d
TobyMayn <tobiasmayn@gmail.com>
2025-12-24 10:08:13 +0100

    implement section 6.1

--- src/main.cpp
+++ src/main.cpp
@@ -6,40 +6,19 @@
 **********************************************************************/
 #include <Arduino.h>
 
-const byte ledPins[] = {4, 2, 15};//define red, green, blue led pins
-const byte chns[] = {0, 1, 2};//define the pwm channels
+#define PIN_BUZZER 13
+#define PIN_BUTTON 4
 
-int red, green, blue;
-
-void setup() {
-    for (int i = 0; i < 3; i++) {
-      //setup the pwm channels,1KHz,8bit
-      ledcAttachChannel(ledPins[i], 500, 8, chns[i]);
-    }
+void setup(){
+  pinMode(PIN_BUZZER, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT);
 }
 
-void setColor(long rgb) {
-    ledcWrite(chns[0], 255 - (rgb >> 16) & 0xFF); //Common anode LED, low level to turn on the led.
-    ledcWrite(chns[1], 255 - (rgb >> 8) & 0xFF);
-    ledcWrite(chns[2], 255 - (rgb >> 0) & 0xFF);
-}
-
-long wheel(int pos) {
-  long WheelPos = pos % 0xff;
-  if (WheelPos < 85) {
-    return ((255 - WheelPos * 3) << 16) | ((WheelPos * 3) << 8);
-  } else if (WheelPos < 170) {
-    WheelPos -= 85;
-    return (((255 - WheelPos * 3) << 8) | (WheelPos * 3));
+void loop(){
+  if(digitalRead(PIN_BUTTON) == LOW){
+    digitalWrite(PIN_BUZZER, HIGH);
   } else {
-    WheelPos -= 170;
-    return ((WheelPos * 3) << 16 | (255 - WheelPos * 3));
+    digitalWrite(PIN_BUZZER, LOW);
   }
 }
 
-void loop() {
-  for (int i = 0; i < 256; i++){
-    setColor(wheel(i));
-    delay(20);
-  }
-}
