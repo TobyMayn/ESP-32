265374829ba901a547248a72ceed1b9caecd5913
TobyMayn <tobiasmayn@gmail.com>
2025-12-24 10:29:21 +0100

    first try on getting section 6.2 to work

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,33 @@
 
 #define PIN_BUZZER 13
 #define PIN_BUTTON 4
+#define CHN 0
 
 void setup(){
   pinMode(PIN_BUZZER, OUTPUT);
   pinMode(PIN_BUTTON, INPUT);
+  ledcAttachChannel(PIN_BUZZER, 0, 10, CHN);
+  ledcWriteTone(CHN, 2000);
+  delay(300);
+}
+
+void alert(){
+  float sinVal;
+  int toneVal;
+
+  for(int x = 0; x<360; x+=10){
+    sinVal = sin(x * (PI / 180));
+    toneVal = 2000 + sinVal * 500;
+    ledcWriteTone(CHN, toneVal);
+    delay(10);
+  }
 }
 
 void loop(){
   if(digitalRead(PIN_BUTTON) == LOW){
-    digitalWrite(PIN_BUZZER, HIGH);
+    alert();
   } else {
-    digitalWrite(PIN_BUZZER, LOW);
+    ledcWriteTone(CHN, 0);
   }
 }
 
